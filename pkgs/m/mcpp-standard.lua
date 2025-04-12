5fc90a42096ebed761bc14bb25e0da1c832fd695
sunrisepeak <speakshen@163.com>
2025-04-12 18:51:02 +0800

    add mcpp-standard project

---
+++ pkgs/m/mcpp-standard.lua
@@ -0,0 +1,9 @@
+package = {
+    -- base info
+    name = "mcpp-standard",
+    description = "Modern Cpp: Core Language Feature Execises",
+
+    authors = "sunrisepeak",
+    license = "Apache-2.0, CC-BY-NC-SA-4.0",
+    repo = "https://github.com/Sunrisepeak/mcpp-standard",
+}

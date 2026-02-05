b816538d3dab47454d565b0cabcb1719925b794d
sunrisepeak <speakshen@163.com>
2026-02-06 00:28:03 +0800

    add d2mystl & mcpp-start

---
+++ pkgs/d/d2mystl.lua
@@ -0,0 +1,9 @@
+package = {
+    -- base info
+    name = "d2mystl",
+    description = "从零实现一个迷你STL库",
+
+    authors = "mcpp-community",
+    license = "Apache-2.0, CC-BY-NC-SA-4.0",
+    repo = "https://github.com/mcpp-community/d2mystl",
+}
---
+++ pkgs/m/mcpp-start.lua
@@ -0,0 +1,9 @@
+package = {
+    -- base info
+    name = "mcpp-start",
+    description = "现代C++启动! - import std",
+
+    authors = "mcpp-community",
+    license = "Apache-2.0, CC-BY-NC-SA-4.0",
+    repo = "https://github.com/mcpp-community/mcpp-start",
+}
